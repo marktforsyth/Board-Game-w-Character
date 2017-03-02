8b5fce5c889ea91a26fa7a85fff26ff7e88d2054
Mark Forsyth <shadowfire168@outlook.com>
2017-03-02 13:08:57 +0100

    updated to support XP system

--- Board.py
+++ Board.py
@@ -23,30 +23,35 @@ all_enemies = [{
     "health": 5,
     "char": "I",
     "damage": 10,
-    "min-lvl": 0
+    "min-lvl": 0,
+    "exp": 5
   }, 
   {
     "health": 8,
     "char": "G",
     "damage": 16,
-    "min-lvl": 0
+    "min-lvl": 0,
+    "exp": 10
   },
   {
     "health": 13,
     "char": "D",
     "damage": 26,
-    "min-lvl": 2
+    "min-lvl": 2,
+    "exp": 15
   },
   {
     "health": 15,
     "char": "P",
     "damage": 30,
-    "min-lvl": 4
+    "min-lvl": 4,
+    "exp": 17
   },
   {
     "health": 18,
     "char": "N",
     "damage": 36,
-    "min-lvl": 8
+    "min-lvl": 8,
+    "exp": 20
   },
 ]
